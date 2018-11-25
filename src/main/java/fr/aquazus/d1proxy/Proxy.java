4d01dbb9ec1ea40b6c685aeb5701b2acf4e25a53
Aquazus <me@aquazus.fr>
2018-11-25 08:06:15 +0100

    Version 1.9.0

--- src/main/java/fr/aquazus/d1proxy/Proxy.java
+++ src/main/java/fr/aquazus/d1proxy/Proxy.java
@@ -29,7 +29,7 @@ public class Proxy {
     }
 
     @Getter
-    private final String version = "1.8.1-dev";
+    private final String version = "1.9.0";
     @Getter
     private ProxyConfiguration configuration;
     @Getter
