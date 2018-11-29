df07cff08482abee0d2a52c320bab456d5ac1a96
Aquazus <me@aquazus.fr>
2018-11-29 14:43:07 +0100

    Autojoin now have a random delay

--- src/main/java/fr/aquazus/d1proxy/handlers/GtHandler.java
+++ src/main/java/fr/aquazus/d1proxy/handlers/GtHandler.java
@@ -2,14 +2,22 @@ package fr.aquazus.d1proxy.handlers;
 
 import fr.aquazus.d1proxy.Proxy;
 import fr.aquazus.d1proxy.network.ProxyClient;
+import lombok.extern.slf4j.Slf4j;
 import simplenet.packet.Packet;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Slf4j
 public class GtHandler implements PacketHandler {
 
     private final Proxy proxy;
+    private ExecutorService scheduler;
 
     public GtHandler(Proxy proxy) {
         this.proxy = proxy;
+        this.scheduler = Executors.newCachedThreadPool();
     }
 
     @Override
@@ -22,7 +30,14 @@ public class GtHandler implements PacketHandler {
         ProxyClient client = proxy.getClientByCharacterId(id);
         if (client != null && client.isAutoJoinEnabled() && client.getCharacterId() != proxyClient.getCharacterId() && client.getGroupLeader() == proxyClient.getGroupLeader() && client.getIp().equals(proxyClient.getIp())) {
             String joinPacket = "GA903" + id + ";" + id;
-            Packet.builder().putBytes(joinPacket.getBytes()).putByte(10).putByte(0).writeAndFlush(proxyClient.getServer());
+            scheduler.execute(() -> {
+                try {
+                    Thread.sleep(ThreadLocalRandom.current().nextInt(1000, 3000));
+                    Packet.builder().putBytes(joinPacket.getBytes()).putByte(10).putByte(0).writeAndFlush(proxyClient.getServer());
+                } catch (Exception ex) {
+                    log.error("An error occurred while executing a game action", ex);
+                }
+            });
         }
         return true;
     }
