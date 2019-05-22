2f0b4e0e713a210d9f1cfe02ec805b1a2eb63acb
Aquazus <me@aquazus.fr>
2019-05-22 08:15:53 +0200

    Windows script update

--- start-windows.bat
+++ start-windows.bat
@@ -2,14 +2,17 @@
 title D1Proxy
 java -version 1>nul 2>nul || (
    echo Error: Java is not installed or cannot be found in your path
+   echo.
    pause
    exit /b
 )
 for /f tokens^=2-5^ delims^=.-_^" %%j in ('java -fullversion 2^>^&1') do set "jver=%%j%%k%%l%%m"
 if not "%jver:~0,2%" == "11" (
    echo Error: D1Proxy can only run on Java 11
+   echo Please make sure the Java in your system path is version 11 or greater
+   echo.
    pause
    exit /b
 )
-java -jar d1proxy-1.10.3-jar-with-dependencies.jar
+java -Dfile.encoding=UTF-8 -jar d1proxy-1.10.4-dev.jar
 pause
